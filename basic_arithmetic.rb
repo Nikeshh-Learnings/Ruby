637d98606c55acfc9d9e9b639d0b718e9c225907
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-07-12 20:07:11 -0400

    Basic Arithmetic operations

---
+++ basic_arithmetic.rb
@@ -0,0 +1,23 @@
+puts 1 + 4
+puts -6 + 13
+puts 10 - 6
+puts 3 * 4
+puts 3 * 4 * 5
+
+# Order of operations: PEMDAS or BODMAS
+# Parentheses
+# Exponents
+# Multiplication
+# Division
+# Addition
+# Subtraction
+puts (1 + 4) * 3
+
+puts 10 / 5
+puts 12 / 5
+
+puts 10 % 3 # % is modulus
+puts 12 % 5
+
+puts 12.0 / 5 # Float division
+puts 16.0 / 5.0
